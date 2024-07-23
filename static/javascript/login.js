b3ce362b7863b64b265a8cd88844e36254ce09cd
Ahmed haytham <95971046+sleekwitit@users.noreply.github.com>
2024-07-23 21:06:44 +0300

    Delete static/javascript/login.js

--- static/javascript/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-            var alertMessage = "wrong email or password, please enter again";
-            if (alertMessage) {
-                alert(alertMessage);
-            }
-        });
