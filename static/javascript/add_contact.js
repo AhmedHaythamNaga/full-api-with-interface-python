00dbe1188687351a5588602796fd96dab62bd4d5
Ahmed haytham <95971046+sleekwitit@users.noreply.github.com>
2024-07-25 01:54:57 +0300

    Delete static directory

--- static/javascript/add_contact.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const returnBtn = document.getElementById("returnToContacts");
-
-    returnBtn.addEventListener("click", function() {
-        window.location.href = "/contacts";
-        alert("contact added successfully");
-    });
-});
